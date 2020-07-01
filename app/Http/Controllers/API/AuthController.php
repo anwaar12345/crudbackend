9d07ee9cbdfbe315fad6e961baa0526a1f29723f
anwarshah <syed.anwar@nextgeni.dev>
2020-07-01 12:53:33 +0500

    soacrud Kick Started

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+use App\User;
+use Validator;
+
+
+class AuthController extends Controller
+{
+    //
+}
